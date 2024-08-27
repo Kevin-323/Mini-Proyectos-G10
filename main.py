74326f176042b0a6b122c931a5fe1cfd9ef36950
Kevin-323 <kaq.utn@gmail.com>
2024-08-27 18:29:03 -0300

    Creacion_ventana_p

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
+import time
+
+# Main window setup
+
+ventana = tk.Tk()
+ventana.title('Grupo 10')
+ventana.geometry('600x450')
+ventana.resizable(False, False)
+ventana.config(bg="#f4f4f4")
+
+
+####    Loop principal
+ventana.mainloop()
